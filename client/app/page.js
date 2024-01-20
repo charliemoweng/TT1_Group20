0d39f67f02b382709cd7c6f2ae919b7ff4ae7f83
Papattarada Apithanangsiri <papattarada1643@gmail.com>
2024-01-20 09:26:08 +0800

    Feat: boilerplate

---
+++ client/app/page.js
@@ -0,0 +1,13 @@
+import { Github } from 'lucide-react'
+import { Button } from '@/components/ui/button'
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Hello world!</h1>
+      <Button variant="outline">Click me</Button>
+    </div>
+  )
+}
+
+export default Home
