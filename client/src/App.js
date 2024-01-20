477aa991c06b9eb414cb60c043f13c06f1c1a767
yamyamz <yaminisezhiyan@gmail.com>
2024-01-20 13:53:22 +0800

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,19 @@
-import './App.css';
-import ItineraryEdit from './ItineraryEdit';
+
 import Login from './Pages/Login';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import DashboardTable from './components/DashboardTable'
-import CreateDestination from './components/CreateDestination';
-import './App.css'
-import DashboardTable from './components/DashboardTable'
+import CreateDestination from './page/CreateDestination';
 import "./App.css";
 import ItineraryEdit from "./ItineraryEdit";
 import CreateItinerary from "./page/CreateItinerary";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import DashboardTable from "./components/DashboardTable";
+
+
 
 function App() {
   const handleSubmit = () => {};
 
   const createItinerary = async () => {};
+  const createDestination = async () => {};
 
   return (
     <Router>
@@ -23,7 +21,7 @@ function App() {
         <Route path='/edit/:id' index element={<ItineraryEdit />} />
         <Route path='/login' element={<Login />} />
         <Route path="/dashboard" elemet={<DashboardTable/>} />
-        <Route path="/createdestination" elemet={<CreateDestination/>} />
+        <Route path="/createDestination" element={<CreateDestination/>} />
     
         <Route path="/createItinerary" index element={<CreateItinerary />} />
         {/* <DashboardTable /> */}
