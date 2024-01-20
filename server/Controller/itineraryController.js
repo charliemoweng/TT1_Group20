f9e16d11a14f5f4ae340493a0c70d73c8b5cc28d
Mengzhe Wang <mengzhe_wang@u.nus.edu>
2024-01-20 13:28:05 +0800

    add itinerary post

--- server/Controller/itineraryController.js
+++ server/Controller/itineraryController.js
@@ -1,9 +1,31 @@
-export const getAllItineraryById = async(req, res) => {
+import Itinerary from "../Models/Itinerary";
+
+
+export const getAllItineraryByUserId = async(req, res) => {
     try{
-        const {id} = req.params;
-        // var resContent= await 
+        const {userId} = req.params;
+        var resContent= await Itinerary.findAll({
+            where: {
+                user_id: userId,
+              }
+        })
         res.status(200).json(resContent);
     } catch {
         res.status(404).json({message: err.message})
     }
+}
+
+export const createItinerary = async (req, res) => {
+    try{
+        // const {userId} = req.params;
+        var resContent= await Itinerary.create({
+            country_name: req.body.country_name,
+            user_id: req.body.user_id,
+            budget: req.body.budget,
+            title: req.body.title
+        })
+        res.status(200).json(resContent);
+    } catch {
+        res.status(404).json({message: err.message})
+    } 
 }
