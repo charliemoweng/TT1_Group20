106f60e6ad0cae03fc2719073c85d554124262df
Papattarada Apithanangsiri <papattarada1643@gmail.com>
2024-01-20 11:46:14 +0800

    Feat: css setup

---
+++ client/src/components/DashboardTable.js
@@ -0,0 +1,34 @@
+const DashboardTable = () => {
+  return (
+    <table class="table-auto">
+      <thead>
+        <tr>
+          <th>Itinerary title</th>
+          <th>Budget</th>
+          <th>Country</th>
+          <th>List of destinations</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>The Sliding Mr. Bones (Next Stop, Pottersville)</td>
+          <td>Malcolm Lockyer</td>
+          <td>1961</td>
+          <td>test destinations</td>
+        </tr>
+        <tr>
+          <td>Witchy Woman</td>
+          <td>The Eagles</td>
+          <td>1972</td>
+        </tr>
+        <tr>
+          <td>Shining Star</td>
+          <td>Earth, Wind, and Fire</td>
+          <td>1975</td>
+        </tr>
+      </tbody>
+    </table>
+  )
+}
+
+export default DashboardTable
