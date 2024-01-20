2830e822eab6d7431907ba1a12c08be17cdf2808
Darren Lim <darrenlimqinyi@hotmail.com>
2024-01-20 15:14:14 +0800

    feat: created service layer for dummy record

--- client/src/components/Itinerary/ItineraryForm.jsx
+++ client/src/components/Itinerary/ItineraryForm.jsx
@@ -1,29 +1,37 @@
 import { createClaim } from "../../service/Itinerary";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { getDestinationByCountry } from "../../service/Itinerary";
 
 export default function ItineraryForm({ action }) {
-  const arrayOfCountries = ["Singapore", "Malaysia", "Indonesia"];
+  const arrayOfCountries = ["Singapore", "Malaysia", "Korea", "Vietnam"];
 
   const [title, setTitle] = useState("");
-  const [countryName, setCountryName] = useState("");
-  const [destination, setDestination] = useState("");
+  const [countryName, setCountryName] = useState("Singapore");
+  const [destinations, setDestinations] = useState([""]);
+  const [selectedDestination, setSelectedDestination] = useState("");
   const [budget, setBudget] = useState(0);
 
   const handleSubmit = () => {
-    if (action == "create") {
+    if (action === "create") {
       createItinerary();
-    } else if (action == "edit") {
+    } else if (action === "edit") {
       // Edit Itinerary
     }
   };
 
-  console.log("Action: " + action);
-
   const createItinerary = async () => {
+    const toJson = {
+      country_name: countryName,
+      user_id: localStorage.getItem("userId"),
+      budget: 100,
+      title: title,
+    };
+
     try {
-      const response = await createClaim();
+      const response = await createClaim(toJson);
+      console.log("Response 1: " + response);
       if (response) {
-        console.log("Response: " + response);
+        console.log("Response 2: " + response);
       } else {
         console.log("Not sure why empty but no error");
       }
@@ -32,6 +40,25 @@ export default function ItineraryForm({ action }) {
       console.log("Error: " + error);
     }
   };
+
+  const retrieveDestinationByCountry = async () => {
+    try {
+      const response = await getDestinationByCountry(countryName);
+      if (response) {
+        setDestinations(response.destination);
+      }
+    } catch (error) {
+      console.log("Error: " + error);
+    }
+  };
+
+  useEffect(() => {
+    getDestinationByCountry();
+  });
+
+  useEffect(() => {
+    retrieveDestinationByCountry();
+  }, [countryName]);
 
   return (
     <div className="flex justify-center items-center bg-slate-400">
@@ -42,28 +69,41 @@ export default function ItineraryForm({ action }) {
             className="border border-slate-400"
             type="text"
             onChange={(e) => setTitle(e.target.value)}
+            required
           />
         </div>
 
         <div>
           <label htmlFor="itinerary-country-name">Country Name: </label>
-          <select>
+          <select onChange={(e) => setCountryName(e.target.value)}>
             {arrayOfCountries &&
-              arrayOfCountries.map((country) => <option>{country}</option>)}
+              arrayOfCountries.map((country) => (
+                <option key={country} value={country}>
+                  {country}
+                </option>
+              ))}
           </select>
         </div>
 
         <div>
           <label htmlFor="itinerary-destination">Destination: </label>
-          <select>
-            <option>Destination 1</option>
-            <option>Destination 2</option>
+          <select onChange={(e) => setSelectedDestination(e.target.value)}>
+            {destinations.map((destination) => (
+              <option key={destination} value={destination}>
+                {destination}
+              </option>
+            ))}
           </select>
         </div>
 
         <div>
           <label htmlFor="itinerary-budget">Budget: </label>
-          <input className="border border-slate-400" type="number" />
+          <input
+            className="border border-slate-400"
+            type="number"
+            onChange={(e) => setBudget(e.target.value)}
+            required
+          />
         </div>
 
         <div>
