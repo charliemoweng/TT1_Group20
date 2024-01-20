88aa2876b830781d3b5d56af999e05db7c67a1cd
Darren Lim <darrenlimqinyi@hotmail.com>
2024-01-20 11:48:41 +0800

    feat: base code for create itinerary

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
+  const handleSubmit = () => {};
+
+  const createItinerary = async () => {};
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex justify-center items-center bg-slate-400">
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="itinerary-title">Title: </label>
+          <input className="border border-slate-400" type="text" />
+        </div>
+
+        <div>
+          <label htmlFor="itinerary-country-name">Country Name: </label>
+          <select>
+            <option>Country 1</option>
+            <option>Country 2</option>
+          </select>
+        </div>
+
+        <div>
+          <label htmlFor="itinerary-destination">Destination: </label>
+          <select>
+            <option>Destination 1</option>
+            <option>Destination 2</option>
+          </select>
+        </div>
+
+        <div>
+          <label htmlFor="itinerary-budget">Budget: </label>
+          <input className="border border-slate-400" type="number" />
+        </div>
+
+        <div>
+          <button type="submit" className="border border-slate-500">
+            Create
+          </button>
+        </div>
+      </form>
     </div>
   );
 }
---
+++ client/src/page/CreateItinerary.jsx
@@ -0,0 +1,7 @@
+export default function CreateItinerary() {
+  return (
+    <div>
+      <p>Create Itineraryyyy</p>
+    </div>
+  );
+}
