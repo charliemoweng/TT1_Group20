e39f019ea538d560541c3fe8b0770f1a220d5bda
yamyamz <yaminisezhiyan@gmail.com>
2024-01-20 13:53:25 +0800

    Update DestinationForm.jsx

---
+++ client/src/components/Destination/DestinationForm.jsx
@@ -0,0 +1,70 @@
+import CreateDestination from "../../page/CreateDestination";
+import { createClaim } from "../../service/Destination";
+import { useState } from "react";
+
+export default function ItineraryForm() {
+  const arrayOfDestinations = ["Singapore", "Malaysia", "Indonesia"];
+
+  const [title, setTitle] = useState("");
+  const [countryName, setCountryName] = useState("");
+  const [destination, setDestination] = useState("");
+  const [budget, setBudget] = useState(0);
+
+  const handleSubmit = () => {
+    CreateDestination();
+  };
+
+  const CreateDestination = async () => {
+    try {
+      const response = await createClaim();
+      if (response) {
+        console.log("Response: " + response);
+      } else {
+        console.log("Not sure why empty but no error");
+      }
+    } catch (error) {
+      // Throw an error
+      console.log("Error: " + error);
+    }
+  };
+
+  return (
+    <div className="flex justify-center items-center bg-slate-400">
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="itinerary-title">Destination Name: </label>
+          <input
+            className="border border-slate-400"
+            type="text"
+            onChange={(e) => setTitle(e.target.value)}
+          />
+        </div>
+
+        <div>
+          <label htmlFor="itinerary-country-name">Country Name: </label>
+          <select>
+            {arrayOfDestinations &&
+              arrayOfDestinations.map((country) => <option>{country}</option>)}
+          </select>
+        </div>
+
+      
+        <div>
+          <label htmlFor="itinerary-cost">Cost: </label>
+          <input className="border border-slate-400" type="number" />
+        </div>
+
+        <div>
+          <label htmlFor="itinerary-budget">Budget: </label>
+          <input className="border border-slate-400" type="number" />
+        </div>
+
+        <div>
+          <button type="submit" className="border border-slate-500">
+            Create
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+}
