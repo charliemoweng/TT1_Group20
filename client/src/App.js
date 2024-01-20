bf988f800450c9da320d7837dc76d6dbae8988d6
Papattarada Apithanangsiri <papattarada1643@gmail.com>
2024-01-20 12:46:12 +0800

    Feat (dashboard): skeleton ui with test code

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
-import logo from './logo.svg'
 import './App.css'
 import DashboardTable from './components/DashboardTable'
 
 function App() {
   return (
     <div className="App">
-      <div className="flex flex-auto bg-red-300">
+      <div className="bg-slate-600 p-4 rounded-sm">
+        <h3 className="font-bold  text-white">Itinerary</h3>
+      </div>
+      <div className="">
         <DashboardTable />
       </div>
     </div>
