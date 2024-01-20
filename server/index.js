6c965ea94c05893d9f8bec3e50fdbcc1a98cd9c5
Ethan Wong <>
2024-01-20 11:12:58 +0800

    remove mongoose connection

--- server/index.js
+++ server/index.js
@@ -18,12 +18,4 @@ app.use(cors());
 
 //ROUTES
 
-// MONGOOSE SETUP
-const PORT = process.env.PORT || 6001;
-mongoose.connect(process.env.MONGO_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-}).then(() => {
-    app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
-})
-.catch((error) => console.log(`${error} did not connect`));
+// MY SQL SET UP WITH SEQUALIZE
