0ccbacc150f8f959ab0cde11e4053c12c562ace4
yamyamz <yaminisezhiyan@gmail.com>
2024-01-20 13:53:28 +0800

    Create CreateDestination.jsx

---
+++ client/src/page/CreateDestination.jsx
@@ -0,0 +1,10 @@
+import DestinationForm from "../components/Destination/DestinationForm";
+
+export default function CreateDestination() {
+  return (
+    <div>
+      <p>Create Destination</p>
+      <DestinationForm />
+    </div>
+  );
+}
