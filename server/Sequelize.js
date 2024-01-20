e0b5ed28967e72604ba0a9447001984cc0bc4239
Ethan Wong <>
2024-01-20 15:43:47 +0800

    remove password

--- server/Sequelize.js
+++ server/Sequelize.js
@@ -2,7 +2,7 @@ import { Sequelize } from "sequelize";
 const sequelize = new Sequelize(
     'DBSSeed',
     'root',
-    'EWsgs323k',
+    'password',
      {
        host: '127.0.0.1',
        dialect: 'mysql'
