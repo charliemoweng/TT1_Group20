98e8b7f1d9389fd601d4a427af80df9b24c63001
yamyamz <yaminisezhiyan@gmail.com>
2024-01-20 12:49:28 +0800

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import ItineraryEdit from './ItineraryEdit';
 import Login from './Pages/Login';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import DashboardTable from './components/DashboardTable'
+import Destination from './components/Destination';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path='/edit/:id' index element={<ItineraryEdit />} />
         <Route path='/login' element={<Login />} />
         <Route path="/dashboard" elemet={<DashboardTable/>} />
+        <Route path="/destination" elemet={<Destination/>} />
       </Routes>
     </Router>
   );
