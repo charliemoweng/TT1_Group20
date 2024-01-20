457e7c8a9c55af955bbf1d23ece5b13f1f8ce486
yamyamz <yaminisezhiyan@gmail.com>
2024-01-20 11:54:45 +0800

    Create Login.js

---
+++ client/src/Pages/Login.js
@@ -0,0 +1,26 @@
+//import './App.css';
+
+function Login() {
+  return (
+    
+    <div className="loginContainer">
+    <label>Email:</label>
+    <input
+      type="text"
+      
+      
+    />
+    <label>Password:</label>
+    <input
+      type="text"
+      />
+
+<button > Login </button>
+
+    </div>
+
+
+  );
+}
+
+export default Login;
